70fd8969999aaea79652f9f5b501bbd5715ee738
Rick welte <rktvpr@gmail.com>
2022-12-07 18:22:47 -0500

    heroku fix v2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const noteData = require('./db/db.json');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
